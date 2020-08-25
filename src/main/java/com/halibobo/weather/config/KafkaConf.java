1849b2e8aac1360d87c35496a821b270376c0498
lizibo <lizibo@jd.com>
2020-08-25 18:49:01 +0800

    add mysql

--- src/main/java/com/halibobo/weather/config/KafkaConf.java
+++ src/main/java/com/halibobo/weather/config/KafkaConf.java
@@ -2,6 +2,12 @@ package com.halibobo.weather.config;
 
 public class KafkaConf {
 
+    /**
+     * Timeout expired while fetching topic metadata
+     * kafka service.properties 修改 host.name 相关
+     * 看kafka客户端与服务端版本是否一致
+     */
+
     public final static String TOPIC = ConfigSolver.getKafkaConfig().getString("topic");
     public final static String APP = ConfigSolver.getKafkaConfig().getString("consumer");
     public final static String ZK_ADDRESS = ConfigSolver.getKafkaConfig().getString("zookeeper");
