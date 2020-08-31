d76af07bf7a49e579a79fa53289dad777a55c3ba
lizibo <lizibo@jd.com>
2020-08-31 11:47:40 +0800

    change some thing

--- src/main/java/com/halibobo/weather/pojo/Weather.java
+++ src/main/java/com/halibobo/weather/pojo/Weather.java
@@ -3,22 +3,57 @@ package com.halibobo.weather.pojo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.base.Objects;
 import com.halibobo.weather.JacksonUtil;
-
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * cat Weather| uniq | sort | awk '{print "private String " $1 " ;"}'
+ *
+ *  1 // DateTimeFormatter - SimpleDateFormat
+ *  2 // Intant - Date
+ *  3 // LocalDateTime - Calendar
+ *  4
+ *  5 // date与instant的相互转化
+ *  6 Instant now = Instant.now();
+ *  7 Date date = Date.from(now);
+ *  8 Instant instant = date.toInstant();
+ *  9 // LocalDateTime代替Calendar
+ * 10 ZoneId zoneId = ZoneId.systemDefault();
+ * 11 LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zoneId);
+ * 12 int year = localDateTime.getYear();
+ * 13 int month = localDateTime.getMonthValue();
+ * 14 int dayOfMonth = localDateTime.getDayOfMonth();
+ * 15 int hour = localDateTime.getHour();
+ * 16 int minute = localDateTime.getMinute();
+ * 17 int second = localDateTime.getSecond();
+ * 18 // DateTimeFormatter代替SimpleDateFormat
+ * 19 DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+ * 20 // Date格式化成String
+ * 21 String format = dateTimeFormatter.format(localDateTime);
+ * 22 LocalDateTime parse = LocalDateTime.parse(format, dateTimeFormatter);
+ * 23 ZoneOffset offset = OffsetDateTime.now().getOffset();
+ * 24 Instant instant2 = parse.toInstant(offset);
+ * 25 // 从String获得Date
+ * 26 Date from = Date.from(instant2);
  */
 public class Weather implements Serializable {
 
-    private final static SimpleDateFormat sdf;
+
+    private final static DateTimeFormatter DATE_TIME_FORMATTER;
     public final transient static Weather EMTPY;
 
     static {
-        sdf = new SimpleDateFormat("yyyy-MM-dd HH:MM");
+        DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd:HH");
         EMTPY = new Weather();
         EMTPY.setPyName("empty");
+        EMTPY.setRtm(DATE_TIME_FORMATTER.format(Instant.EPOCH.atZone(TimeZone.getDefault().toZoneId()).toLocalDateTime()));
     }
 
     static final long serialVersionUID = 126789345698L;
@@ -56,7 +91,7 @@ public class Weather implements Serializable {
     private String rtm;
 
     public Weather() {
-        setRtm(sdf.format(System.currentTimeMillis()));
+        setRtm(DATE_TIME_FORMATTER.format(Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime()));
     }
 
     public String getTem2() {
@@ -237,5 +272,25 @@ public class Weather implements Serializable {
             e.printStackTrace();
         }
         return EMTPY;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Weather.EMTPY);
+        ThreadLocal<SimpleDateFormat> SDF = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd:HH"));
+        ExecutorService ts = Executors.newFixedThreadPool(100);
+        for (; ; ) {
+            ts.execute(() -> {
+                try {
+                   String s = SDF.get().format(Date.from(Instant.now()));
+                   System.out.println(s);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }finally {
+                   SDF.remove();
+                }
+            });
+
+        }
     }
 }
